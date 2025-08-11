e7c706ab0a34182e9e4b4b360239ec42f7afc5c3
rens <rensroedema@gmail.com>
2025-08-11 13:28:31 +0200

    mijn-website

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+(function(){
+  const path = location.pathname.split('/').pop() || 'index.html';
+  document.querySelectorAll('header.site .link').forEach(a => {
+    const href = a.getAttribute('href');
+    if((path === '' && href === 'index.html') || href === path){
+      a.classList.add('active');
+    }
+  });
+})();
